e6d8b9b5e0c1665fd9ca3fd0a6f8ba55a554d3b3
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2024-03-23 14:46:10 +0530

    Add files via upload

---
+++ (Input) Program to read the record of n no. of students and display them using array of structure in tabular form.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+struct student{
+int rollnumber;
+char name[15];
+char branch[12];
+float marks;
+};
+int main(){
+int size,i;
+printf("enter array size:");
+scanf("%d",&size);
+struct student slist[size];
+for(i=0;i<size;i++){
+printf("enter rollnumber:");
+scanf("%d",&slist[i].rollnumber);
+printf("enter name:");
+fflush(stdin);
+gets(slist[i].name);
+printf("enter branch:");
+gets(slist[i].branch);
+printf("enter marks:");
+scanf("%f",&slist[i].marks);
+}
+printf("student records are:\n");
+for(i=0;i<size;i++){
+printf("%15d %15s %15s %15.2f\n",slist[i].rollnumber,slist[i].name,slist[i].branch,slist[i].marks);
+}
+return 0;
+}
